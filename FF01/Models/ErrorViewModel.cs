d141aaf0495c11461008ecbbe07cbe9305e81871
Camilla <camilla@littlegiants.dk>
2019-06-01 20:56:59 +0200

    Add project files.

---
+++ FF01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FF01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
